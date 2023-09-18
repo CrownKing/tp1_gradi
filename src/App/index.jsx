3be3a346d31bff366131ccfec1b72db87b857a10
Jonathan Lopes <jonathanljs0@gmail.com>
2023-09-17 22:10:07 -0300

    dynamoDB incluido no codigo porem com erro

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -8,6 +8,39 @@ import Integracoes from "./organisms/Integracoes";
 import { StoreProvider } from "./store";
 
 
+//Import Dynamo
+import * as AWS from 'aws-sdk';
+
+AWS.config.update({
+    region: 'us-east-2',
+    endpoint: 'dynamodb.us-east-2.amazonaws.com',
+    accessKeyId: ['AKIAVOTPS6UVT3WP5SM6'],
+    secretAccessKey: ['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']
+  });
+
+  const onRead = () => {
+    
+    var dynamodB = new AWS.DynamoDB();
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    
+        let that = this;
+        let params = {
+            TableName: "gradi"
+        };
+    
+        docClient.scan(params, function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+        //     that.setState({
+        //         gridData: data
+        // })
+        console.log({data})
+        }
+    });
+    };
+    onRead()
+
 export const routes = [
     { path: '/home', icon: HomeIcon, tilte: "Home", import: Home },
     { path: '/dashboard', icon: DashIcon, tilte: "Dashboard", import: Dashboard },
@@ -20,6 +53,9 @@ export default function App() {
             <div class="h-full flex">
                 <Sidebar />
                 <main>
+                <Grid data={this.state.gridData}>
+        // Grid columns
+    </Grid>
                     <Routes>
                         <For each={routes}>
                             {route => (
